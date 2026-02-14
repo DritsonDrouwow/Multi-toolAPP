20ea37d1a59d0f44a810688d020b9105f00acd02
DritsonDrouwow <goodeman75@gmail.com>
2026-02-14 10:44:44 +0800

    deploy_app

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os 
-from dotenv import load_dotennv
+from dotenv import load_dotenv
 
-load_dotennv()
+load_dotenv()
 
 HF_API_KEY = os.getenv("HF_API_KEY", "")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
